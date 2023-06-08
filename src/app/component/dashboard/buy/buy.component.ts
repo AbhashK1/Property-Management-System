ed6d3a1b6cefff3b203a6ca9deadca12689a610f
Aayush <aayushbisht501@gmail.com>
2023-06-08 18:43:42 +0530

    added filter in buy property

--- src/app/component/dashboard/buy/buy.component.ts
+++ src/app/component/dashboard/buy/buy.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { Property } from 'src/app/shared/model/property';
 import { DataService } from 'src/app/shared/service/data.service';
+import { MatSelectModule } from '@angular/material/select';
 
-export interface Tile{
-  id:string;
-  cost:string;
-  location:string;
+export interface Tile {
+  id: string;
+  cost: string;
+  location: string;
+  type: string;
 }
 
 @Component({
@@ -14,36 +16,62 @@ export interface Tile{
   templateUrl: './buy.component.html',
   styleUrls: ['./buy.component.css']
 })
-export class BuyComponent implements OnInit{
-
-  propertyArr: Property[] =[];
+export class BuyComponent implements OnInit {
+  propertyArr: Property[] = [];
   tiles: Tile[] = [];
+  filteredTiles: Tile[] = [];
+
+  costFilter: number | null = null;
+  locationFilter: string | null = null;
+  typeFilter: string | null = null;
+
   constructor(
-    public grid:MatGridListModule,
-    private dataApi:DataService,
-  ){}
+    public grid: MatGridListModule,
+    private dataApi: DataService,
+  ) {}
+
   ngOnInit(): void {
     this.getAllProperty();
   }
 
-  viewproperty(id:any){
-    window.open('/dashboard/sell/'+id,'_blank');
+  viewproperty(id: any) {
+    window.open('/dashboard/sell/' + id, '_blank');
   }
 
-  getAllProperty(){
-    this.dataApi.getAllProperty().subscribe(res =>{
-      this.propertyArr=res.map((e:any)=>{
+  getAllProperty() {
+    this.dataApi.getAllProperty().subscribe(res => {
+      this.propertyArr = res.map((e: any) => {
         const data = e.payload.doc.data();
-        data.id=e.payload.doc.id;
+        data.id = e.payload.doc.id;
         return data;
-      })
-      //console.log(this.propertyArr);
-      for(let i=0;i<this.propertyArr.length;i++){
-        var id=this.propertyArr[i].id;
-        var cost=this.propertyArr[i].cost;
-        var location=this.propertyArr[i].location;
-        this.tiles.push({id,cost,location});
+      });
+
+      for (let i = 0; i < this.propertyArr.length; i++) {
+        const id = this.propertyArr[i].id;
+        const cost = this.propertyArr[i].cost;
+        const location = this.propertyArr[i].location;
+        const type = this.propertyArr[i].type;
+        this.tiles.push({ id, cost, location, type });
       }
-    })
+
+      this.filteredTiles = this.tiles; // Initialize filteredTiles with all tiles
+    });
   }
+
+  selectedFilter: string = 'cost';
+  filterValue: string = '';
+  
+  applyFilters(): void {
+    this.filteredTiles = this.tiles.filter((tile: Tile) => {
+      if (this.selectedFilter === 'cost') {
+        return tile.cost.includes(this.filterValue);
+      } else if (this.selectedFilter === 'location') {
+        return tile.location.toLowerCase().includes(this.filterValue.toLowerCase());
+      } else if (this.selectedFilter === 'type') {
+        return tile.type.toLowerCase().includes(this.filterValue.toLowerCase());
+      }
+      return true;
+    });
+  }
+  
 }
